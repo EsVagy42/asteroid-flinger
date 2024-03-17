ad37b85b5db11a31af3145a777c404933f2baab8
&|| <sltamagotchi@gmail.com>
2024-03-17 12:39:46 +0000

    Added ModFloat

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use bevy::prelude::*;
 
+mod mod_float;
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
---
+++ src/mod_float.rs
@@ -0,0 +1,97 @@
+use core::ops::*;
+
+#[derive(Debug, Copy, Clone, PartialEq)]
+pub struct ModFloat(f64);
+
+const MODULO: f64 = 512.0;
+const MODULO_HALF: f64 = MODULO / 2.0;
+
+impl ModFloat {
+    pub fn new(x: f64) -> Self {
+        ModFloat((x + MODULO_HALF).rem_euclid(MODULO) - MODULO_HALF)
+    }
+
+    pub fn abs(&self) -> Self {
+        if self.0 < 0.0 {
+            ModFloat::new(-self.0)
+        } else {
+            *self
+        }
+    }
+}
+
+impl Add for ModFloat {
+    type Output = Self;
+    fn add(self, other: Self) -> Self {
+        ModFloat::new(self.0 + other.0)
+    }
+}
+
+impl Sub for ModFloat {
+    type Output = Self;
+    fn sub(self, other: Self) -> Self {
+        ModFloat::new(self.0 - other.0)
+    }
+}
+
+impl Mul for ModFloat {
+    type Output = Self;
+    fn mul(self, other: Self) -> Self {
+        ModFloat::new(self.0 * other.0)
+    }
+}
+
+impl Div for ModFloat {
+    type Output = Self;
+    fn div(self, other: Self) -> Self {
+        ModFloat::new(self.0 / other.0)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_mod_addition() {
+        assert_eq!(
+            ModFloat::new(255.) + ModFloat::new(1.),
+            ModFloat::new(-256.)
+        );
+    }
+
+    #[test]
+    fn test_mod_subtraction() {
+        assert_eq!(
+            ModFloat::new(-256.) - ModFloat::new(1.),
+            ModFloat::new(255.)
+        );
+    }
+
+    #[test]
+    fn test_mod_multiplication() {
+        assert_eq!(
+            ModFloat::new(128.) * ModFloat::new(2.),
+            ModFloat::new(-256.)
+        );
+    }
+
+    #[test]
+    fn test_mod_division() {
+        assert_eq!(
+            ModFloat::new(128.) / ModFloat::new(0.5),
+            ModFloat::new(-256.)
+        );
+    }
+
+    #[test]
+    fn test_mod_abs() {
+        assert_eq!(ModFloat::new(-128.).abs(), ModFloat::new(128.));
+        assert_eq!(ModFloat::new(-256.).abs(), ModFloat::new(-256.));
+    }
+
+    #[test]
+    fn test_new() {
+        assert_eq!(ModFloat::new(256.), ModFloat::new(-256.));
+    }
+}
