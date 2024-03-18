54159a7c897c45b694c3e9bb98fecb44b7428b88
&|| <sltamagotchi@gmail.com>
2024-03-18 17:56:06 +0000

    Added spaceship atlas

--- src/general_systems.rs
+++ src/general_systems.rs
@@ -2,19 +2,26 @@ use bevy::prelude::*;
 
 use crate::player::Player;
 
-pub fn startup(mut commands: Commands, asset_server: Res<AssetServer>) {
+pub fn startup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut texture_atlas_layouts: ResMut<Assets<TextureAtlasLayout>>,
+) {
     commands.spawn(Camera2dBundle::default());
+    let texture_atlas_layout = texture_atlas_layouts.add(TextureAtlasLayout::from_grid(Vec2::new(8., 8.), 3, 1, None, None));
     commands.spawn((
         Player,
-        SpriteBundle {
+        SpriteSheetBundle {
             sprite: Sprite {
-                custom_size: Some(Vec2::new(32., 32.)),
+                custom_size: Some(Vec2::new(32.0, 32.0)),
                 ..Default::default()
             },
             texture: asset_server.load("spaceship.png"),
-            transform: Transform::from_translation(Vec3::new(0., 0., 1.)),
+            atlas: TextureAtlas {
+                layout: texture_atlas_layout,
+                index: 0,
+            },
             ..Default::default()
         },
     ));
-
 }
