dfc1d7bb1c15b2133163f79119d175ac126d57bf
&|| <sltamagotchi@gmail.com>
2024-04-03 22:16:29 +0000

    Added tests for collision

--- src/game/collider.rs
+++ src/game/collider.rs
@@ -1,6 +1,6 @@
-use bevy::prelude::*;
 use crate::game::components::Position;
 use crate::game::wrap;
+use bevy::prelude::*;
 
 #[derive(Component)]
 pub struct CircleCollider {
@@ -8,7 +8,32 @@ pub struct CircleCollider {
 }
 
 impl CircleCollider {
-    pub fn collides(&self, position: &Position, other: &CircleCollider, other_position: &Position) -> bool {
-        wrap::wrap_vec2(position.0 - other_position.0).length_squared() <= (self.radius + other.radius).powi(2)
+    pub fn collides(
+        &self,
+        position: &Position,
+        other: &CircleCollider,
+        other_position: &Position,
+    ) -> bool {
+        wrap::wrap_vec2(position.0 - other_position.0).length_squared()
+            <= (self.radius + other.radius).powi(2)
+    }
+}
+
+#[cfg(test)]
+mod collider_tests {
+    use super::*;
+
+    #[test]
+    fn test_collides() {
+        assert!(CircleCollider { radius: 1. }.collides(
+            &Position(Vec2::new(-1024., -1024.)),
+            &CircleCollider { radius: 1. },
+            &Position(Vec2::new(1023., 1023.))
+        ));
+        assert!(!CircleCollider { radius: 1. }.collides(
+            &Position(Vec2::new(-1023., -1023.)),
+            &CircleCollider { radius: 1. },
+            &Position(Vec2::new(1023., 1023.))
+        ));
     }
 }
